46fbdac7d2a01f8a8d7be8f3d4deb7ab95d06923
blendthink <admin@blendthink.dev>
2022-07-03 16:58:31 +0900

    use simplecov-cobertura

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,8 @@ require "simplecov"
 SimpleCov.start
 
 if ENV["CI"] == "true"
-  require "codecov"
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+  require "simplecov-cobertura"
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
 
 require "bundler/setup"
